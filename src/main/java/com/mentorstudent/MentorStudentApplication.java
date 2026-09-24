4edc782d40687e33aa4c8eacb8f7f9b424d1c3b2
aksharareddy487-cloud <aksharareddy487@gmail.com>
2026-09-24 11:44:16 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/mentorstudent/MentorStudentApplication.java
@@ -0,0 +1,13 @@
+package com.mentorstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorStudentApplication.class, args);
+	}
+
+}
